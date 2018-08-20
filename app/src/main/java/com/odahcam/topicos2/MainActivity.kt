711150528c0d178e19c87f2c3435fa1540bf4430
Luiz Filipe Machado Barni <machado@odahcam.com>
2018-08-20 00:42:21 -0300

    pan

--- app/src/main/java/com/odahcam/topicos2/MainActivity.kt
+++ app/src/main/java/com/odahcam/topicos2/MainActivity.kt
@@ -3,16 +3,29 @@ package com.odahcam.topicos2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        attachButtonOnClickListener();
     }
 
     fun openTranslateActivity() {
         val intent = Intent(this, TranslateActivity::class.java)
         startActivity(intent)
     }
+
+    fun attachButtonOnClickListener() {
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            openTranslateActivity()
+        }
+    }
+
 }
+
